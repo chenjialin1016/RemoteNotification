b83f03489c29e610362738b9197901ba1645044b
chenjialin <chenjialin@chenjialindemac.local>
2016-06-20 10:53:28 +0800

    Initial Commit

---
+++ push/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  push
+//
+//  Created by chenjialin on 16/6/20.
+//  Copyright © 2016年 chenjialin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ push/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  push
+//
+//  Created by chenjialin on 16/6/20.
+//  Copyright © 2016年 chenjialin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
